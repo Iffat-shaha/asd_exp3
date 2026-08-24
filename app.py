fe043ac2b5eca83f34df4998c052d7c9ba64bc1f
Iffat-shaha <r240018@famt.ac.in>
2026-08-24 23:29:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-print("Hello from Jenkins Tutorial 3")
-print("file modified")
-print("file modified..2")
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def home():
+ return "Hello! This application was deployed using Jenkins."
+if __name__ == "__main__":
+ app.run(host="0.0.0.0", port=5000)
